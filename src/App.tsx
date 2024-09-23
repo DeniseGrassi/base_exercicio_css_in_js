189540b2986fc6fc138709ba43807ef97d1e8859
Denise Grassi <denygrassi_@hotmail.com>
2024-09-23 08:56:59 -0300

    atualizacao styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import Header from './components/Cabecalho'
+import { ThemeProvider } from 'styled-components'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import themeOrigin from './themes/ThemeProvider'
+
+import Cabecalho from './components/Cabecalho'
+
+import EstiloGlobal, { Container } from './styledGlobal'
 
 function App() {
   return (
-    <>
-      <Header />
+    <ThemeProvider theme={themeOrigin}>
+      <EstiloGlobal />
+      <Cabecalho />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
